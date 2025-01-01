102e44b5579da0b4d87c0664562cd6d3bdbdf9a4
Mats Larsen <mats@jun.codes>
2025-01-01 16:25:41 +0900

    feat: add expr and stmt variants to hir

--- compiler/hachi-hir/src/expr.rs
+++ compiler/hachi-hir/src/expr.rs
@@ -6,6 +6,16 @@ use hachi_syntax::Span;
 pub enum HirExpr<'hir> {
     IntegerLiteral(HirIntegerLiteralExpr<'hir>),
     BooleanLiteral(HirBooleanLiteralExpr<'hir>),
+    Assign(HirAssignExpr<'hir>),
+    UnaryOp(HirUnaryOpExpr<'hir>),
+    BinaryOp(HirBinaryOpExpr<'hir>),
+    AddressOf(HirAddressOfExpr<'hir>),
+    Deref(HirDerefExpr<'hir>),
+    ConstantIndex(HirConstantIndexExpr<'hir>),
+    OffsetIndex(HirOffsetIndexExpr<'hir>),
+    Call(HirCallExpr<'hir>),
+    Construct(HirConstructExpr<'hir>),
+    Group(HirGroupExpr<'hir>),
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -22,4 +32,112 @@ pub struct HirBooleanLiteralExpr<'hir> {
     pub span: Span,
     pub value: bool,
     pub ty: InternedTy<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirAssignExpr<'hir> {
+    pub span: Span,
+    pub lhs: HirExpr<'hir>,
+    pub rhs: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirUnaryOpExpr<'hir> {
+    pub span: Span,
+    pub operand: HirExpr<'hir>,
+    pub op: HirUnaryOp,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirBinaryOpExpr<'hir> {
+    pub span: Span,
+    pub lhs: HirExpr<'hir>,
+    pub rhs: HirExpr<'hir>,
+    pub op: HirBinaryOp,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirAddressOfExpr<'hir> {
+    pub span: Span,
+    pub inner: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirDerefExpr<'hir> {
+    pub span: Span,
+    pub inner: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirConstantIndexExpr<'hir> {
+    pub span: Span,
+    pub origin: HirExpr<'hir>,
+    pub index: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirOffsetIndexExpr<'hir> {
+    pub span: Span,
+    pub origin: HirExpr<'hir>,
+    pub index: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirCallExpr<'hir> {
+    pub span: Span,
+    pub callee: HirExpr<'hir>,
+    pub arguments: Vec<HirExpr<'hir>>,
+    pub type_arguments: Vec<InternedTy<'hir>>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirConstructExpr<'hir> {
+    pub span: Span,
+    pub callee: InternedTy<'hir>,
+    pub arguments: Vec<HirExpr<'hir>>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirGroupExpr<'hir> {
+    pub span: Span,
+    pub inner: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub enum HirUnaryOp {
+    Not,
+    Neg,
+    Deref,
+    AddressOf,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub enum HirBinaryOp {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Rem,
+    Eq,
+    Neq,
+    Lt,
+    Gt,
+    Le,
+    Ge,
+    Lte,
+    Gte,
+    And,
+    Or,
 }
--- compiler/hachi-hir/src/stmt.rs
+++ compiler/hachi-hir/src/stmt.rs
@@ -1,11 +1,17 @@
 use crate::expr::HirExpr;
-use crate::HirName;
+use crate::{HirName, InternedTy};
 use hachi_syntax::Span;
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
 #[derive(Debug)]
 pub enum HirStmt<'hir> {
     Let(HirLetStmt<'hir>),
+    Loop(HirLoopStmt<'hir>),
+    Expr(HirExprStmt<'hir>),
+    Return(HirReturnStmt<'hir>),
+    If(HirIfStmt<'hir>),
+    Break(HirBreakStmt<'hir>),
+    Continue(HirContinueStmt<'hir>),
 }
 
 #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
@@ -15,4 +21,54 @@ pub struct HirLetStmt<'hir> {
     pub name: HirName<'hir>,
     pub r#type: InternedTy<'hir>,
     pub value: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirIfStmt<'hir> {
+    pub span: Span,
+    pub condition: HirExpr<'hir>,
+    pub happy_path: Vec<HirStmt<'hir>>,
+    pub unhappy_path: Vec<HirStmt<'hir>>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirExprStmt<'hir> {
+    pub span: Span,
+    pub expr: HirExpr<'hir>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirLoopStmt<'hir> {
+    pub span: Span,
+    /// The initializer for the loop, if any.
+    ///
+    /// TODO: Consider if this should not be lowered into a let statement
+    pub initializer: Option<HirLetStmt<'hir>>,
+    /// The condition for the loop to continue.
+    ///
+    /// For infinite loops, this node should be a constant literal of boolean true.
+    pub condition: HirExpr<'hir>,
+    pub body: Vec<HirStmt<'hir>>,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirBreakStmt<'hir> {
+    pub span: Span,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirContinueStmt<'hir> {
+    pub span: Span,
+}
+
+#[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
+#[derive(Debug)]
+pub struct HirReturnStmt<'hir> {
+    pub span: Span,
+    pub value: Option<HirExpr<'hir>>,
 }
