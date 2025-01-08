b5b2e4eb4b2daf5e5f57a82e92ebf7302ab0012a
Mats Larsen <mats@jun.codes>
2025-01-09 00:11:58 +0900

    chore: rename fields in HirArena

--- compiler/eight-hir/src/arena.rs
+++ compiler/eight-hir/src/arena.rs
@@ -16,77 +16,86 @@ use std::collections::HashMap;
 /// It also simplifies comparison of types to pointer equality.
 #[derive(Debug)]
 pub struct HirArena<'a> {
-    arena: &'a Bump,
-    type_store: RefCell<HashMap<HirTyId, &'a HirTy<'a>>>,
+    allocator: &'a Bump,
+    interned_types: RefCell<HashMap<HirTyId, &'a HirTy<'a>>>,
 }
 
 impl<'arena> HirArena<'arena> {
     pub fn new(bump: &'arena Bump) -> Self {
         Self {
-            arena: bump,
-            type_store: RefCell::new(HashMap::new()),
+            allocator: bump,
+            interned_types: RefCell::new(HashMap::new()),
         }
     }
 
     /// Get a type from the arena.
     pub fn get_type(&self, id: HirTyId) -> Option<&HirTy> {
-        self.type_store.borrow().get(&id).copied()
+        self.interned_types.borrow().get(&id).copied()
     }
 
     pub fn get_pointer_ty(&self, ty: &'arena HirTy) -> &HirTy {
         let id = HirTyId::compute_pointer_ty_id(&HirTyId::from(ty));
-        self.type_store
+        self.interned_types
             .borrow_mut()
             .entry(id)
-            .or_insert_with(|| self.arena.alloc(HirTy::Pointer(HirPointerTy { inner: ty })))
+            .or_insert_with(|| {
+                self.allocator
+                    .alloc(HirTy::Pointer(HirPointerTy { inner: ty }))
+            })
     }
 
     pub fn get_nominal_ty(&self, name: &HirName) -> &HirTy {
         let id = HirTyId::compute_nominal_ty_id(name.name.as_str());
-        self.type_store.borrow_mut().entry(id).or_insert_with(|| {
-            self.arena
-                .alloc(HirTy::Nominal(HirNominalTy { name: name.clone() }))
-        })
+        self.interned_types
+            .borrow_mut()
+            .entry(id)
+            .or_insert_with(|| {
+                self.allocator
+                    .alloc(HirTy::Nominal(HirNominalTy { name: name.clone() }))
+            })
     }
 
     pub fn get_integer32_ty(&self) -> &HirTy {
         let id = HirTyId::compute_integer32_ty_id();
-        self.type_store
+        self.interned_types
             .borrow_mut()
             .entry(id)
-            .or_insert_with(|| self.arena.alloc(HirTy::Integer32(HirInteger32Ty {})))
+            .or_insert_with(|| self.allocator.alloc(HirTy::Integer32(HirInteger32Ty {})))
     }
 
     pub fn get_boolean_ty(&self) -> &HirTy {
         let id = HirTyId::compute_boolean_ty_id();
-        self.type_store
+        self.interned_types
             .borrow_mut()
             .entry(id)
-            .or_insert_with(|| self.arena.alloc(HirTy::Boolean(HirBooleanTy {})))
+            .or_insert_with(|| self.allocator.alloc(HirTy::Boolean(HirBooleanTy {})))
     }
 
     pub fn get_unit_ty(&self) -> &HirTy {
         let id = HirTyId::compute_unit_ty_id();
-        self.type_store
+        self.interned_types
             .borrow_mut()
             .entry(id)
-            .or_insert_with(|| self.arena.alloc(HirTy::Unit(HirUnitTy {})))
+            .or_insert_with(|| self.allocator.alloc(HirTy::Unit(HirUnitTy {})))
     }
 
     pub fn get_uninitialized_ty(&self) -> &HirTy {
         let id = HirTyId::compute_uninitialized_ty_id();
-        self.type_store.borrow_mut().entry(id).or_insert_with(|| {
-            self.arena
-                .alloc(HirTy::Uninitialized(HirUninitializedTy {}))
-        })
+        self.interned_types
+            .borrow_mut()
+            .entry(id)
+            .or_insert_with(|| {
+                self.allocator
+                    .alloc(HirTy::Uninitialized(HirUninitializedTy {}))
+            })
     }
 
     pub fn get_variable_ty(&self, var: usize) -> &HirTy {
         let id = HirTyId::compute_variable_ty_id(var);
-        self.type_store
+        self.interned_types
             .borrow_mut()
             .entry(id)
-            .or_insert_with(|| self.arena.alloc(HirTy::Variable(HirVariableTy { var })))
+            .or_insert_with(|| self.allocator.alloc(HirTy::Variable(HirVariableTy { var })))
     }
 
     pub fn get_function_ty(
@@ -100,11 +109,14 @@ impl<'arena> HirArena<'arena> {
             .collect::<Vec<_>>();
         let return_type_id = HirTyId::from(return_type);
         let id = HirTyId::compute_function_ty_id(&return_type_id, parameter_ids.as_slice());
-        self.type_store.borrow_mut().entry(id).or_insert_with(|| {
-            self.arena.alloc(HirTy::Function(HirFunctionTy {
-                return_type,
-                parameters,
-            }))
-        })
+        self.interned_types
+            .borrow_mut()
+            .entry(id)
+            .or_insert_with(|| {
+                self.allocator.alloc(HirTy::Function(HirFunctionTy {
+                    return_type,
+                    parameters,
+                }))
+            })
     }
 }
