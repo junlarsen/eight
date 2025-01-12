e2b667a67cfdd73175c3ca22b148f1e9a598c1bd
Mats Larsen <mats@jun.codes>
2025-01-13 04:26:49 +0900

    test: add compiler ui test driven by lit

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -8,6 +8,6 @@ config.suffixes = ['.eight']
 config.test_source_root = os.path.dirname(__file__)
 
 config.substitutions.append(('%eightc', os.path.join(os.getcwd(), 'target', 'debug', 'eightc')))
-
+config.substitutions.append(('%not', os.path.join(os.getcwd(), 'target', 'debug', 'eight-not')))
 # TODO: Consider searching for compatible FileCheck versions on the system instead.
 config.substitutions.append(('%fc', 'FileCheck-15'))
