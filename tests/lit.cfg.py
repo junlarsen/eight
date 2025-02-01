bd201651fcdc764cd23e50cfd2f98139bedf9a3a
Mats Jun Larsen <mats@jun.codes>
2025-02-02 01:47:55 +0900

    fix: use default FileCheck on PATH

--- tests/lit.cfg.py
+++ tests/lit.cfg.py
@@ -10,4 +10,4 @@ config.test_source_root = os.path.dirname(__file__)
 config.substitutions.append(('%eightc', os.path.join(os.getcwd(), 'target', 'debug', 'eightc')))
 config.substitutions.append(('%not', os.path.join(os.getcwd(), 'target', 'debug', 'eight-not')))
 # TODO: Consider searching for compatible FileCheck versions on the system instead.
-config.substitutions.append(('%fc', 'FileCheck-15'))
+config.substitutions.append(('%fc', 'FileCheck'))
